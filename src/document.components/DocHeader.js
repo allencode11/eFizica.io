96124d9b760f1073dc86297e19e71d7bbf98eab8
Andrei Pavalachi <apavalac@Andreis-MacBook-Air.local>
2022-01-31 23:12:58 +0200

    edit pdf structure

--- src/document.components/DocHeader.js
+++ src/document.components/DocHeader.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Text, View } from '@react-pdf/renderer';
 import data from '../jsonData/data.json';
-import { styles} from './DocTable/DocTableStyles';
 
 export const DocHeader = (str) => (
   <View>
@@ -9,27 +8,29 @@ export const DocHeader = (str) => (
       flexDirection: "row",
       justifyContent: "space-between",
       marginBottom: 10,
-      fontSize: 16,
+      fontSize: 10,
+      textAlign: 'center',
     }}>
       <Text>Disciplina: Fizica</Text>
+      <Text>Data___________________ </Text>
       <Text>Clasa a VII-a </Text>
     </View>
 
     <View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 10, fontSize: 12}}>
-      <Text>Nume, prenume student: __________________</Text>
+      <Text>Nume, prenume elev: __________________</Text>
+      <Text>Punctaj total:_____ </Text>
       <Text>Punctaj acumulat:_____ </Text>
       <Text>Nota: _____</Text>
-
     </View>
-    <Text style={styles.content}>{ 'Unitatea:' + data[0].module}</Text>
     <View style={{
       flexDirection: "row",
       justifyContent: "space-between",
-      marginBottom: 10, fontSize: 12, color: 'red',
+      marginBottom: 5, fontSize: 12, color: 'red',
     }}>
       <Text>Nume, prenume profesor: __________________</Text>
       <Text>Semnatura ______________</Text>
     </View>
+    <Text style={{ textAlign: 'center', fontSize: 12, marginBottom: 10 }}>{ 'Unitatea:' + data[0].module}</Text>
 
   </View>
 )
